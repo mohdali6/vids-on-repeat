81112deaa319faf85e6be8b3bb085a392e53dc78
Mohd Ali <mohdali6@users.noreply.github.com>
2015-09-08 13:58:46 +0530

    Added module having functions for calling YouTube api, modified views

---
+++ vids_on_repeat/api_functions.py
@@ -0,0 +1,29 @@
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import os
+import json
+
+keyFile = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/some_data.json'
+
+YOUTUBE_API_KEY = ""
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+with open(keyFile, 'r') as f:
+    keys = json.load(f)
+    if 'youtube_api_key' in keys:
+        YOUTUBE_API_KEY = keys['youtube_api_key']
+
+def youtube_search(**kwargs):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_API_KEY)
+    search_response = youtube.search().list(q=kwargs['query'], part="id,snippet",
+                                            maxResults=kwargs['max_results']).execute()
+
+    videos = []
+
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+              videos.append((search_result["snippet"]["title"], search_result["id"]["videoId"]))
+
+    return videos
--- vids_on_repeat/views.py
+++ vids_on_repeat/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .forms import VideoSearchForm
+from .api_functions import youtube_search
+
+MAX_RESULTS = '15'
 
 def index(request):
     form = VideoSearchForm()
@@ -9,6 +12,8 @@ def index(request):
 
 def video_search(request):
     if 'search_query' in request.GET and request.GET['search_query']:
+        videoList = youtube_search(query=request.GET['search_query'], max_results=MAX_RESULTS)
+        print videoList
         return HttpResponse("Display search results")
     else:
         return redirect('/vids/')
