46cd46434b02b9d09b6e6dc6b5e5a905ca79d039
Mohd Ali <mohdali6@users.noreply.github.com>
2015-09-09 10:25:55 +0530

    Index page template error handling added and in views api call put in try/except block

--- vids_on_repeat/views.py
+++ vids_on_repeat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from apiclient.errors import HttpError
 
 from .forms import VideoSearchForm
 from .api_functions import youtube_search
@@ -8,12 +9,23 @@ MAX_RESULTS = '15'
 
 def index(request):
     form = VideoSearchForm()
-    return render(request, 'vids_on_repeat/index.html', {'form': form})
+    context = {'form': form}
+
+    if 'error' in request.session.keys() and request.session['error'] == True:
+        request.session['error'] = False
+        context = {'form': form, 'error': True}
+
+    return render(request, 'vids_on_repeat/index.html', context)
 
 def video_search(request):
     if 'search_query' in request.GET and request.GET['search_query']:
-        videoList = youtube_search(query=request.GET['search_query'], max_results=MAX_RESULTS)
-        print videoList
+        try:
+            videoList = youtube_search(query=request.GET['search_query'], max_results=MAX_RESULTS)
+        except HttpError, e:
+            print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+            request.session['error'] = True
+            return redirect('/vids/')
+
         return HttpResponse("Display search results")
     else:
         return redirect('/vids/')
