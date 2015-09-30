0406d4a8acafafff0c7ea935b762f14ee8dde5eb
Mohd Ali <mohdali6@users.noreply.github.com>
2015-09-30 13:30:30 +0530

    Add incremet repeat function, changed AJAX call method to PUT and added response codes

--- vids_on_repeat/views.py
+++ vids_on_repeat/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from apiclient.errors import HttpError
 from models import Video, VideoRepeats
-
 from .forms import VideoSearchForm
 from .api_functions import youtube_search, video_title_search
 from django.db.models import F
 import json
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 MAX_RESULTS = '15'
 WATCH_URL = '/vids/watch/'
@@ -36,6 +36,7 @@ def video_search(request):
         return redirect('/vids/')
 
 
+@ensure_csrf_cookie
 def watch_video(request, video_id):
     try:
         video_title = video_title_search(video_id)
@@ -46,7 +47,7 @@ def watch_video(request, video_id):
     request.session['player_requested'] = True
 
     videoSearchForm = VideoSearchForm()
-    context = {'videoId': video_id, 'videoSearchForm':videoSearchForm}
+    context = {'videoId': video_id, 'videoSearchForm': videoSearchForm}
 
     return render(request, 'vids_on_repeat/watch.html', context)
 
@@ -56,20 +57,19 @@ def most_repeated_video(request):
     return JsonResponse({'video_id': video_id})
 
 
-#Function to increment overall repeats of a video by 1  #http://127.0.0.1:8000/vids/watch/ZEdUljT7eGI/
+# Function to increment overall repeats of a video by 1  #http://127.0.0.1:8000/vids/watch/ZEdUljT7eGI/
+@ensure_csrf_cookie
 def increment_repeat(request):
     if request.method == 'POST':
         video_id = json.loads(request.body)['video_id']
         try:
-            vid, created = VideoRepeats.objects.get_or_create(video__video_id=video_id)
-            #vid = VideoRepeats.objects.get(video__video_id=video_id)
+            vid, created = VideoRepeats.objects.get_or_create(video=Video.objects.get(video_id=video_id))
             vid.repeat_count = F('repeat_count') + 1
             vid.save()
         except:
-            pass
+            return HttpResponse(status=500)
 
-    #ToDo Change response
-    return redirect('/vids/')
+    return HttpResponse(status=204)
 
 
 #ToDo Function that returns list of top 5 trends
