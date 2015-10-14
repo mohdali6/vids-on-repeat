2aca26fc2bef99d5d101f6828dc0eeb8be086325
Mohd Ali <mohdali6@users.noreply.github.com>
2015-10-14 16:14:06 +0530

    Add OpenAura API functions

--- vids_on_repeat/api_functions.py
+++ vids_on_repeat/api_functions.py
@@ -3,6 +3,7 @@ from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import os
 import json
+import requests
 
 ## ********** Part of this code is from YouTube API samples, which are covered under Apache 2.0 License ********** ##
 
@@ -12,10 +13,18 @@ YOUTUBE_API_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
+# OpenAura API
+
+OPEN_AURA_KEY = ""
+OPEN_AURA_ARTIST_SEARCH = "http://api.openaura.com/v1/search/artists?"
+
+
 with open(keyFile, 'r') as f:
     keys = json.load(f)
     if 'youtube_api_key' in keys:
         YOUTUBE_API_KEY = keys['youtube_api_key']
+    OPEN_AURA_KEY = str(keys.get('open_aura_key'))
+
 
 def youtube_search(**kwargs):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_API_KEY)
@@ -30,8 +39,16 @@ def youtube_search(**kwargs):
 
     return videos
 
+
 def video_title_search(video_id):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_API_KEY)
     video_response = youtube.videos().list(id=video_id,
                                            part='snippet').execute()
     return video_response["items"][0]["snippet"]["title"]
+
+
+# Searches Open Aura taking artist name as query parameter
+def open_aura_search(query):
+    open_aura_artist_search_url = OPEN_AURA_ARTIST_SEARCH + "q=" + query + "&api_key=" + OPEN_AURA_KEY
+    r = requests.get(open_aura_artist_search_url)
+    return r.json()
